2f624c80f6b33fed6885516b4a1271f4a5155e4f
Stivo <23172201+stivoat@users.noreply.github.com>
2023-05-10 22:41:19 +0200

    Replace tailwind png with svg that adjusts to font size

--- src/decorations.ts
+++ src/decorations.ts
@@ -1,7 +1,27 @@
-import { DecorationRangeBehavior, window } from "vscode"
-import path = require("path")
-import { Settings } from "./configuration"
+import { DecorationRangeBehavior, Uri, window, workspace } from "vscode"
 import * as Config from "./configuration"
+import { Settings } from "./configuration"
+import path = require("path")
+import fs = require("fs")
+
+const svgPath = path.resolve(__dirname, "../images/tailwindicon.svg")
+const svgContent = fs.readFileSync(svgPath, "utf8")
+
+function generateScaledSVGPath() {
+    const fontSize = workspace.getConfiguration("editor").get("fontSize") as number
+    const scaleFactor = 0.9
+    const width = fontSize * scaleFactor
+    const height = fontSize * scaleFactor
+
+    const resizedSVGContent = svgContent
+        .replace(/width=".*?"/, `width="${width}"`)
+        .replace(/height=".*?"/, `height="${height}"`)
+
+    const svgBase64 = Buffer.from(resizedSVGContent).toString("base64")
+    const svgDataURI = `data:image/svg+xml;base64,${svgBase64}`
+
+    return Uri.parse(svgDataURI)
+}
 
 export function UnfoldedDecorationType() {
     return window.createTextEditorDecorationType({
@@ -14,9 +34,7 @@ export function FoldedDecorationType() {
     return window.createTextEditorDecorationType({
         before: {
             contentIconPath:
-                Config.get<boolean>(Settings.ShowTailwindImage) === true
-                    ? path.resolve(__dirname, "../images/tailwindicon.png")
-                    : undefined,
+                Config.get<boolean>(Settings.ShowTailwindImage) === true ? generateScaledSVGPath() : undefined,
             backgroundColor: Config.get<string>(Settings.FoldedTextBackgroundColor) ?? "transparent",
             margin: "0 0 0 -5px",
         },
